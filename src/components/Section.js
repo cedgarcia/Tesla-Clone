fd2b6c55ae5419a93c63b8170f3328fa18c7d498
Cedrick Garcia <icedgarcia@gmail.com>
2021-09-21 22:59:02 +0800

    props done wip

---
+++ src/components/Section.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Section = ({ title, description, leftBtnText, rightBtnText })=> {
+  // console.log(props)
+  return (
+    <Sections>
+      <HeroText>
+        <h1>{ title }</h1>
+        <p>{ description }</p>
+      </HeroText>
+
+      <Buttons>
+        <ButtonGroup>
+          <LButton>{ leftBtnText }</LButton>
+          <RButton>{ rightBtnText }</RButton>
+        </ButtonGroup>
+
+        <DownArrow src = './images/down-arrow.svg'/>
+      </Buttons>
+    </Sections>
+  )
+}
+ 
+export default Section
+
+const Sections = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: url(/images/model-s.jpg) no-repeat center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+`
+const HeroText = styled.div`
+  color: red;
+  // background-color: red ;
+  padding: 15vh;
+`
+
+
+const ButtonGroup =styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  @media(max-width: 768px) {
+    flex-direction: column;
+
+  }
+`
+
+const LButton = styled.div`
+  background-color: rgba(23, 26, 32); 
+  margin: 0 1.3rem;
+  height: 48px;
+  width:256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;   
+  border-radius: 100px;
+  // opacity: 0.85; 
+  text-transform: uppercase;
+  font-size: 12px ;
+  cursor: pointer;
+  @media(max-width: 768px) {
+    margin-bottom: 1rem; 
+  }
+`
+const RButton = styled(LButton)`
+  background-color: white;
+  color: black;
+  
+`
+
+const DownArrow = styled.img`
+  margin-top:1rem;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+
+`
+
+const Buttons = styled.div`
+  margin-bottom: 1rem;
+`
